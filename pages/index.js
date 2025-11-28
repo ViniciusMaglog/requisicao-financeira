0f52b3a72ffd74090b3e68e6da3654cbed33a414
Maglog Transporte e Logística <vinicius.oliveira@maglog.com.br>
2025-11-28 11:36:19 -0300

    Update page title to include 'Maglog'

--- pages/index.js
+++ pages/index.js
@@ -146,7 +146,7 @@ export default function ReembolsoPage() {
 
   return (
     <div className="min-h-screen bg-gray-200 dark:bg-gray-900 p-2 md:p-4 font-sans transition-colors duration-200">
-      <Head><title>Requisição Financeira</title></Head>
+      <Head><title>Requisição Financeira - Maglog</title></Head>
       
       <div className="w-full max-w-[98%] md:max-w-[95%] mx-auto bg-white dark:bg-gray-800 p-4 md:p-8 rounded-lg shadow-xl border-t-8 border-cyan-900 dark:border-cyan-600">
         
